dba25f8b57854bacb9b6607ff15c7820bd28c6f6
Eduarda Lima <me.eduuarda@gmail.com>
2025-04-11 21:30:53 -0400

    fix: correção e ajuste no ícone de favorito

--- src/components/GifCardList.vue
+++ src/components/GifCardList.vue
@@ -21,9 +21,7 @@
         />
       </div>
 
-      <div
-        class="absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200 z-20"
-      >
+      <div class="absolute top-2 right-2 z-20">
         <q-btn
           flat
           round
